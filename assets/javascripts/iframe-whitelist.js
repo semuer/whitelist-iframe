f39d103747db4fa5e1de493eb401091b09e10214
semuer <semuer@gmail.com>
2016-02-14 02:53:26 +0900

    Update iframe-whitelist.js

--- assets/javascripts/iframe-whitelist.js
+++ assets/javascripts/iframe-whitelist.js
@@ -3,7 +3,16 @@
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/w\.soundcloud\.com\/player\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/graphics8\.nytimes\.com\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.youtube\.com\/embed\/.+/i);
-  Discourse.Markdown.whiteListIframe(/^(http?:)?\/\/static\.hdslb\.com\/miniloader\.swf\/.+/i);
-  Discourse.Markdown.whiteListIframe(/^(http?:)?\/\/www\.tudou\.com\/v\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/static\.hdslb\.com\/miniloader\.swf\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.tudou\.com\/v\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/v\.qq\.com\/iframe\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/yuntv\.letv\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/v\.17173\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/player\.youku\.com\/embed\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.tudou\.com\/programs\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.56\.com\/iframe\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/tv\.sohu\.com\/upload\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/ssl\.acfun\.tv\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.google\.com\/maps\/d\/embed\?.+/i);
 })();
 
