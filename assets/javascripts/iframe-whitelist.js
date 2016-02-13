715b45275f7d172831d97e9bbd269ae1af5e682b
semuer <semuer@gmail.com>
2016-02-14 01:59:05 +0900

    Update iframe-whitelist.js

--- assets/javascripts/iframe-whitelist.js
+++ assets/javascripts/iframe-whitelist.js
@@ -3,5 +3,7 @@
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/w\.soundcloud\.com\/player\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/graphics8\.nytimes\.com\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.youtube\.com\/embed\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(http?:)?\/\/static\.hdslb\.com\/miniloader\.swf\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(http?:)?\/\/www\.tudou\.com\/v\/.+/i);
 })();
 
